8252648c61e7419b6e577d0b5458b45d19259991
csatsurnh <cshan1995@126.com>
2023-04-19 19:24:43 +0800

    update app.py

--- app.py
+++ app.py
@@ -51,47 +51,6 @@ def inference(input):
         out_file=args.out_file) 
     return output
 
-# from argparse import ArgumentParser
-
-# from mmseg.apis import MMSegInferencer
-
-
-# def inference(input):
-#     parser = ArgumentParser()
-#     # parser.add_argument('img', help='Image file')
-#     parser.add_argument('--model', default='configs/pidnet/pidnet-l_2xb6-120k_1024x1024-cityscapes.py', help='Config file') #noqa
-#     parser.add_argument('--checkpoint', default='https://download.openmmlab.com/mmsegmentation/v0.5/pidnet/pidnet-l_2xb6-120k_1024x1024-cityscapes/pidnet-l_2xb6-120k_1024x1024-cityscapes_20230303_114514-0783ca6b.pth', help='Checkpoint file') #noqa
-#     parser.add_argument(
-#         '--out-dir', default='', help='Path to save result file')
-#     parser.add_argument(
-#         '--show',
-#         action='store_true',
-#         default=False,
-#         help='Whether to display the drawn image.')
-#     parser.add_argument(
-#         '--dataset-name',
-#         default='cityscapes',
-#         help='Color palette used for segmentation map')
-#     parser.add_argument(
-#         '--device', default='cuda:0', help='Device used for inference')
-#     parser.add_argument(
-#         '--opacity',
-#         type=float,
-#         default=0.5,
-#         help='Opacity of painted segmentation map. In (0, 1] range.')
-#     args = parser.parse_args()
-
-#     # build the model from a config file and a checkpoint file
-#     mmseg_inferencer = MMSegInferencer(
-#         args.model,
-#         args.checkpoint,
-#         dataset_name=args.dataset_name,
-#         device=args.device)
-
-#     # test a single image
-#     return(mmseg_inferencer(input, show=args.show, out_dir=args.out_dir, opacity=args.opacity))
-
-
 gr.Interface(
     fn=inference,
     inputs=gr.Image(type='numpy'),
