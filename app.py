1c383fc9939b04810ea773839e6a699668fa3565
csatsurnh <cshan1995@126.com>
2023-04-21 16:46:35 +0800

    update app

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ from mmseg.apis import inference_model, init_model, show_result_pyplot
 def inference(input):
     parser = ArgumentParser()
     # parser.add_argument('img', help='Image file')
-    parser.add_argument('--config', default='configs/segformer/segformer_mit-b0_8xb2-160k_ade20k-512x512.py', help='Config file') #noqa
+    parser.add_argument('--config', default='configs/segformer/segformer_mit-b0_8xb2-160k_ade20k-512x512_app_resize.py', help='Config file') #noqa
     parser.add_argument('--checkpoint', default='https://download.openmmlab.com/mmsegmentation/v0.5/segformer/segformer_mit-b0_512x512_160k_ade20k/segformer_mit-b0_512x512_160k_ade20k_20210726_101530-8ffa8fda.pth', help='Checkpoint file') #noqa
     parser.add_argument('--out-file', default=None, help='Path to output file')
     parser.add_argument(
